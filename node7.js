862a82a8319119208d8d7498a3eab8c63cf2d773
Jyotsnachappali <jyotsnachappali65@gmail.com>
2026-03-12 12:23:02 +0530

    Add files via upload

---
+++ node7.js
@@ -0,0 +1,9 @@
+let adhar = false;
+let pan =true;
+if(adhar){
+    console.log("you can open account in our bank: using adhar");
+}else if(pan){
+    console.log("you can open account in our bank: using pan" );
+}else {
+    console.log("you can not open account in our bank");
+}
