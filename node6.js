2070bad64b097f6972c1613336e1dcce3ab423b2
Jyotsnachappali <jyotsnachappali65@gmail.com>
2026-03-12 12:22:47 +0530

    Add files via upload

---
+++ node6.js
@@ -0,0 +1,9 @@
+let age=28;
+let d1=false;
+if(age > 19 || d1== true ){
+    console.log("you cannot drive");
+}else if(age <19 || d1== false){
+    console.log("you can drive");
+}else {
+    console.log("you cannot drive");
+}
