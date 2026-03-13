6bab308b2f54ce2ec57734eba7ceb5e142a563bf
Jyotsnachappali <jyotsnachappali65@gmail.com>
2026-03-13 12:00:27 +0530

    Add files via upload

---
+++ node11.js
@@ -0,0 +1,11 @@
+let fruits=['apple','banana','orange'];
+let phones=['apple','moto','samsung'];
+let score=[100,89.77,0,78,10];
+let random=[91,'sony',234.78,'@'];
+console.log(fruits[0]);
+console.log(fruits[2]);
+console.log(phones[1]);
+console.log(phones[2]);
+console.log(random[0]);
+console.log(score[2]);
+
